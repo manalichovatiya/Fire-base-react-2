e531f5c0ff9892355461b7d09962c1cac1e86f9f
MANALI CHOVATIYA <manalichovatiya07@gmail.com>
2024-01-24 07:38:13 +0530

    fire base

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getData } from './firebase/function';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   useEffect(() => {
-    console.log("useEffect triggered");
-    getData();
+    const fetchData = async () => {
+      const data = await getData();
+      setTodos(data);
+    };
+    fetchData();
   }, []);
 
   return (
     <div className="App">
       <h1>Todos:</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
